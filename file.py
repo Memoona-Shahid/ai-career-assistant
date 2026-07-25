b92bc07c041569059e6aa9d9395ff34ebbed1fe9
Memoona Shahid <memoonabitlogics@gmail.com>
2026-07-26 00:38:07 +0500

    test

---
+++ file.py
@@ -0,0 +1,11 @@
+PS C:\Users\User\Documents\150 days of AI-ENGINEERING\Ai-career-assistant> git config --local --list
+core.repositoryformatversion=0
+core.filemode=false
+core.bare=false
+core.logallrefupdates=true
+core.symlinks=false
+core.ignorecase=true
+remote.origin.url=https://github.com/Memoona-Shahid/ai-career-assistant.git
+remote.origin.fetch=+refs/heads/*:refs/remotes/origin/*
+branch.main.remote=origin
+branch.main.merge=refs/heads/main
